d252c37b67b90869db819f19f3ca8356a9f3cf04
RunKi <eeezgame@163.com>
2020-08-30 23:19:00 +0800

    TicTacToe！

---
+++ Week_01/TicTacToe/js/util.js
@@ -0,0 +1,11 @@
+
+function opponentColor(color) {
+    switch (color) {
+      case 'black':
+        return 'white';
+      case 'white':
+        return 'black';
+      default:
+        throw new Error(`Unknown color: ${color}`);
+    }
+}
