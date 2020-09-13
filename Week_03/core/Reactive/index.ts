cf5af98ecec8fdccc9b4ad2298d2f62f3a7e0708
翟润棋 <>
2020-09-13 23:33:15 +0800

    :fix

--- Week_03/core/Reactive/index.ts
+++ Week_03/core/Reactive/index.ts
@@ -1,44 +1,48 @@
 
-let effects = new Map()
-let reactivities = new Map()
-let currentEffect:any = null
-
-function effect(fn:any){
-    currentEffect = fn
-    fn()
-    currentEffect =null
-}
-function reactive (object:object) {
-    if (reactivities.has(object)) return reactivities.get(object)
-
-    let observed = new Proxy(object,{
-        get(object:any, propety:string){
-            if(currentEffect){
-                if(!effects.has(object))
-                    effects.set(object,new Map);
-                if(!effects.get(object).has(propety))
-                    effects.get(object).set(propety, new Array)
-                effects.get(object).get(propety).push(currentEffect)
+(function (window:any) {
+    
+    let effects = new Map()
+    let reactivities = new Map()
+    let currentEffect:any = null
+    
+    function effect(fn:any){
+        currentEffect = fn
+        fn()
+        currentEffect =null
+    }
+    function reactive (object:object) {
+        if (reactivities.has(object)) return reactivities.get(object)
+    
+        let observed = new Proxy(object,{
+            get(object:any, propety:string){
+                if(currentEffect){
+                    if(!effects.has(object))
+                        effects.set(object,new Map);
+                    if(!effects.get(object).has(propety))
+                        effects.get(object).set(propety, new Array)
+                    effects.get(object).get(propety).push(currentEffect)
+                }
+                return object[propety]
+            },
+            set(object, propety ,value){
+                object[propety] = value;
+                for(let effect of effects.get(object).get(propety)){
+                    effect()
+                }
+                return true
             }
-            return object[propety]
-        },
-        set(object, propety ,value){
-            object[propety] = value;
-            for(let effect of effects.get(object).get(propety)){
-                effect()
-            }
-            return true
-        }
-    })
-    reactivities.set(object, observed)
-    return observed
-}
+        })
+        reactivities.set(object, observed)
+        return observed
+    }
+    
+    
+    if (typeof window !== 'undefined' && !window.effect) {
+        window.effect = effect
+    }
+    
+    if (typeof window !== 'undefined' && !window.reactive) {
+        window.reactive = reactive
+    }
+})(window);
 
-
-if (typeof window !== 'undefined' && window.effect) {
-    window.effect = effect
-}
-
-if (typeof window !== 'undefined' && window.reactive) {
-    window.reactive = reactive
-}
